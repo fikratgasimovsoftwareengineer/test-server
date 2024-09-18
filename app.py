bb78fde72980c9920488a2a085914107b2842da7
Fikrat <fikretqasimov95@gmail.com>
2024-09-18 14:30:01 +0000

    tesst

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, jsonify
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Example GET route
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {
-        "message": "Hello from Flask API!"
-    }
-    return jsonify(data)
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify(message="Hello, welcome to your Flask API!")
 
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
